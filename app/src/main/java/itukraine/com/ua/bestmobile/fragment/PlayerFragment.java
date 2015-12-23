7d5224e8d1f6c2e6245dd07837d0b4648f3eb8bb
Dytyniak <vadym.dytyniak@gmail.com>
2015-12-23 17:00:14 +0200

    Added buttons for player

--- app/src/main/java/itukraine/com/ua/bestmobile/fragment/PlayerFragment.java
+++ app/src/main/java/itukraine/com/ua/bestmobile/fragment/PlayerFragment.java
@@ -3,10 +3,11 @@ package itukraine.com.ua.bestmobile.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 import itukraine.com.ua.bestmobile.R;
 
@@ -15,6 +16,7 @@ import itukraine.com.ua.bestmobile.R;
  */
 public class PlayerFragment extends Fragment {
 
+    private static final String TAG = PlayerFragment.class.getCanonicalName();
 
     public PlayerFragment() {
         // Required empty public constructor
@@ -23,10 +25,28 @@ public class PlayerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
+        View view = inflater.inflate(R.layout.fragment_player, container, false);
+        final Button buttonPlay = (Button) view.findViewById(R.id.button_play);
+        final Button buttonPause = (Button) view.findViewById(R.id.button_pause);
+        final ProgressBar songProgressbar = (ProgressBar) view.findViewById(R.id.song_progressbar);
 
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonPlay.setVisibility(View.GONE);
+                buttonPause.setVisibility(View.VISIBLE);
+            }
+        });
 
-        return inflater.inflate(R.layout.fragment_player, container, false);
+        buttonPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonPlay.setVisibility(View.VISIBLE);
+                buttonPause.setVisibility(View.GONE);
+            }
+        });
+
+        return view;
     }
 
 }
