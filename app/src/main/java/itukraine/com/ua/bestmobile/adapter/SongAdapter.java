2abaa10a546534ab7859314afbdb982eef96e41e
volodia_chornenkiy <chornenkyy@gmail.com>
2015-12-24 18:47:46 +0200

    Created and displayed list for songs

---
+++ app/src/main/java/itukraine/com/ua/bestmobile/adapter/SongAdapter.java
@@ -0,0 +1,85 @@
+package itukraine.com.ua.bestmobile.adapter;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import itukraine.com.ua.bestmobile.R;
+import itukraine.com.ua.bestmobile.dao.Song;
+import itukraine.com.ua.bestmobile.fragment.PlayerFragment;
+import itukraine.com.ua.bestmobile.util.MusicUtil;
+
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder> {
+
+    private static final String TAG = PlayerFragment.class.getCanonicalName();
+
+    public List<Song> songsInPlaylist;
+    private Context mContext;
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public SongAdapter(Context context, List<Song> songsInPlaylist) {
+        this.songsInPlaylist = songsInPlaylist;
+        mContext = context;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public SongAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_song, parent, false);
+
+        ViewHolder vh = new ViewHolder(v);
+        vh.mSongTitle = (TextView) v.findViewById(R.id.song_title);
+        vh.mSongArtist = (TextView) v.findViewById(R.id.song_artist);
+        vh.mAlbumArt = (ImageView) v.findViewById(R.id.album_art);
+
+        return vh;
+    }
+
+    private Song getItem(int pos) {
+        return songsInPlaylist.get(pos);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mSongTitle.setText(getItem(position).title);
+        holder.mSongArtist.setText(getItem(position).artist);
+        Bitmap albumArt = MusicUtil.getInstance().getAlbumart(mContext, getItem(position).albumId);
+        if (albumArt != null) {
+            holder.mAlbumArt.setImageBitmap(albumArt);
+        } else {
+            holder.mAlbumArt.setImageResource(R.drawable.default_song_picture);
+        }
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return songsInPlaylist.size();
+    }
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mSongTitle;
+        public TextView mSongArtist;
+        public ImageView mAlbumArt;
+
+        public ViewHolder(View v) {
+            super(v);
+        }
+
+    }
+}
--- app/src/main/java/itukraine/com/ua/bestmobile/fragment/SongListFragment.java
+++ app/src/main/java/itukraine/com/ua/bestmobile/fragment/SongListFragment.java
@@ -1,22 +1,74 @@
 package itukraine.com.ua.bestmobile.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 import itukraine.com.ua.bestmobile.R;
+import itukraine.com.ua.bestmobile.adapter.SongAdapter;
+import itukraine.com.ua.bestmobile.dao.Song;
+import itukraine.com.ua.bestmobile.util.MusicUtil;
+import itukraine.com.ua.bestmobile.util.RecyclerItemClickListener;
+import itukraine.com.ua.bestmobile.view.RecyclerViewLineDevider;
 
 public class SongListFragment extends Fragment {
+
+    private static final String TAG = AllPlaylistsFragment.class.getCanonicalName();
+
+    private RecyclerView mRecyclerView;
+    private SongAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private Context mContext;
+
+    private List<Song> songList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_song_list, container, false);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.songs_view);
 
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(mContext);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        songList = MusicUtil.getInstance().getAllSongs(mContext); // TODO load songs from playlist
+
+        mAdapter = new SongAdapter(mContext, songList);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mRecyclerView.addItemDecoration(new RecyclerViewLineDevider(getResources()));
+        mRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(mContext,
+                new OnItemClickListener()));
 
         return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
+    private class OnItemClickListener extends RecyclerItemClickListener.SimpleOnItemClickListener {
+
+        @Override
+        public void onItemClick(View childView, int position) {
+            // TODO open player???
+        }
+
     }
 
 
